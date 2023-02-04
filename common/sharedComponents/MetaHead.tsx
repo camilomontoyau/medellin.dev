ab262813f457368120f4d4f3534cf01b8bda75dc
khriztianmoreno <khriztianmoreno@gmail.com>
2023-02-04 14:17:44 -0500

    feat: :sparkles: add meta tags

--- common/sharedComponents/MetaHead.tsx
+++ common/sharedComponents/MetaHead.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react'
 import Head from 'next/head'
+import { useRouter } from 'next/router';
 
 type MetaHeadProps = {
   title?: string
@@ -7,28 +8,36 @@ type MetaHeadProps = {
 }
 
 const MetaHead: FC<MetaHeadProps> = ({ title, description }) => {
+  const { asPath } = useRouter();
+    const origin =
+        typeof window !== 'undefined' && window.location.origin
+            ? window.location.origin
+            : '';
+
+  const URL = `${origin}${asPath}`;
+
   return (
     <Head>
       <title>{title}</title>
       <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
       <link rel="sitemap" href="sitemap.xml"/>
-      <link rel="icon" href="https://storage.googleapis.com/mixo-files/logos/devMeetups-1675451859350.svg"/>
+      <link rel="icon" href="img/devMeetups.svg"/>
       <meta name="description" content={description} />
-      <meta property="og:url" content="https://www.mixo.io/site/dev-meetups-tjhab"/>
+
+      <meta property="og:url" content={URL} />
       <meta property="og:title" content={title}/>
       <meta property="og:site_name" content="Medellin.dev"/>
       <meta property="og:type" content="website"/>
       <meta property="og:description" content={description} />
-      <meta property="og:image" content="https://storage.googleapis.com/mixo-files/uploads/devmeetups-1675451860499.png"/>
+      <meta property="og:image" content="img/cover.jpg"/>
       <meta property="og:image:alt" content={title}/>
-      <meta property="twitter:url" content="https://www.mixo.io/site/dev-meetups-tjhab"/>
+
+      <meta property="twitter:url" content={URL} />
       <meta property="twitter:title" content={title}/>
       <meta property="twitter:description" content={description} />
       <meta property="twitter:card" content="summary_large_image"/>
-      <meta property="twitter:image:src" content="https://storage.googleapis.com/mixo-files/uploads/devmeetups-1675451860499.png"/>
-      <meta property="generator" content="îles"/>
-      <meta name="head:count" content="18"/>
+      <meta property="twitter:image:src" content="img/cover.jpg"/>
     </Head>
   )
 }
